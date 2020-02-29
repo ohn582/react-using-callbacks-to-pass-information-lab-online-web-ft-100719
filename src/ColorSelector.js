95ebdc2b05cb68aeea1d4ded4c9a3ced5948278e
Neal JIn Oh <no4259@gmail.com>
2020-02-29 21:43:15 +0000

    Automatically backed up by Learn

--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -10,7 +10,7 @@ export default class ColorSelector extends Component {
 
   render() {
     return (
-      <div onClick={callback} key={idx} className="color-swatch" style={{backgroundColor: str}}>
+      <div id="colorSelector" >
         {this.makeColorSwatches()}
       </div>
     )
